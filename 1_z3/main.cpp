295592ed3903eb45ed7e6e4c02d5dd464155a165
Klaudia Filipek <klaudia.filipek@gmail.com>
2012-01-16 20:51:23 +0100

    Dzielenie

---
+++ 1_z3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m, n;
+    int result;
+
+    cout << "m = " << m << endl;
+    cout << "n = " << n << endl;
+
+    result = m/n;
+
+    cout << "Wynik = " << result << endl;
+    cout << "Reszta = " << m%n;
+
+    return 0;
+}
