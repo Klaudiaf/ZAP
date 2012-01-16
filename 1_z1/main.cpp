586cf9b53f0ba30521eb63d72580d151e1d32b71
Klaudia Filipek <klaudia.filipek@gmail.com>
2012-01-16 20:19:19 +0100

    Srednia arytmetyczna

---
+++ 1_z1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+
+    int result = (x+y+z)/3;
+
+    cout << "Wynik = " << result << endl;
+
+    return 0;
+
+}
